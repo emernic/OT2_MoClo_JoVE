22c57698178af5957a5b53bf715d5af1ffb312f5
emernic <emernic@umich.edu>
2018-11-18 00:11:40 -0500

    Fixed issues with moclo transform protocol.

--- ot2_moclo_jove/moclo_transform/data/moclo_transform_template.py
+++ ot2_moclo_jove/moclo_transform/data/moclo_transform_template.py
@@ -2,6 +2,18 @@ import math
 
 from opentrons import robot, instruments, labware, modules
 
+COLD_BLOCK = '96-PCR-tall-cold-block'
+try:
+	labware.create(
+		COLD_BLOCK,
+		grid=(12, 8),
+		spacing=(9, 9),
+		diameter=5,
+		depth=15.4,
+		volume=200)
+except:
+	print("Using existing labware definition for {0}".format(COLD_BLOCK))
+
 # Up to 48 rxns per run (4 spots on agar per rxn)
 # Multichannel p300, single channel p10
 # This protocol is optimized for maximum walkaway time
@@ -26,15 +38,15 @@ dna_plate_map_dict = {}
 dna_plate_map_dict["plate_0"] = gen_part_plate(0)
 dna_plate_map_dict["plate_1"] = gen_part_plate(96)
 
-combinations_to_make = [{"name": "final plasmid 1", "parts": ["vector a", "insert 1", "another insert"]}]
-dna_plate_map_dict = {"dna plate 0": [["vector a", "insert 1", "another insert"]]}
+#combinations_to_make = [{"name": "final plasmid 1", "parts": ["vector a", "insert 1", "another insert"]}]
+#dna_plate_map_dict = {"dna plate 0": [["vector a", "insert 1", "another insert"]]}
 
 num_rxns = len(combinations_to_make)
 num_plates = math.ceil(num_rxns/24)
 
 # Load in 96-well PCR plate (96-PCR-flat) on temp deck for moclos, transformation, and outgrowth.
 temp_deck = modules.load('tempdeck', '10')
-reaction_plate = labware.load('96-PCR-flat', '10', share=True)
+reaction_plate = labware.load('96-PCR-tall', '10', share=True)
 
 available_deck_slots = ['11', '8', '7', '5', '4', '2', '1']
 
@@ -65,15 +77,17 @@ liquid_waste = trough.wells(4)
 # Load in up to 2 DNA plates (96-PCR-flat)
 dna_plate_dict = {}
 for plate_name in dna_plate_map_dict.keys():
-	dna_plate_dict[plate_name] = labware.load('96-PCR-flat', available_deck_slots.pop(), plate_name)
+	dna_plate_dict[plate_name] = labware.load('96-PCR-tall', available_deck_slots.pop(), plate_name)
 
 # Load in comp cell plate (96-PCR-flat)
-comp_cells = labware.load('96-flat', available_deck_slots.pop(), 'Competent cells')
+comp_cells = labware.load(COLD_BLOCK, available_deck_slots.pop(), 'Competent cells')
 
 # Load in up to 2 agar plates, same antibiotic for all plasmids is assumed (e-gelgol)
 agar_plates = []
 for i in range(0, num_plates):
 	agar_plates.append(labware.load('e-gelgol', available_deck_slots.pop(), 'Agar plate {0}'.format(i)))
+
+temp_deck.set_temperature(10)
 
 # Add water, buffer, restriction enzyme, ligase, and buffer to 2x master mix.
 # Add extra space for dead volume.
@@ -92,10 +106,21 @@ while mm_to_make > 0:
 	mm_well = reaction_plate.wells(well_in_plate)
 	well_in_plate -= 1
 	mm_wells.append(mm_well)
-	p10_single.transfer(0.65*vol, water.bottom(), mm_well.bottom(), new_tip='never')
-	p10_single.transfer(vol/5.0, buffer.bottom(), mm_well.bottom(), mix_after=(2, 10), new_tip='never')
-	p10_single.transfer(vol/20.0, ligase.bottom(), mm_well.bottom(), mix_after=(2, 10), new_tip='never')
-	p10_single.transfer(vol/10.0, restriction_enzyme.bottom(), mm_well.bottom(), mix_after=(5, 10), new_tip='never')
+	water_to_transfer = 0.65*vol
+	# Transfer large volumes first to avoid dead volume.
+	while water_to_transfer > 0:
+		if water_to_transfer > 10:
+			p10_single.transfer(10, water.bottom(), mm_well.bottom(0.5), new_tip='never')
+			water_to_transfer -= 10
+		else:
+			p10_single.transfer(water_to_transfer, water.bottom(), mm_well.bottom(0.5), new_tip='never')
+			water_to_transfer = 0
+	p10_single.transfer(vol/5.0, buffer.bottom(), mm_well.bottom(0.5), new_tip='never')
+	p10_single.mix(2, 10, mm_well.bottom(0.5))
+	p10_single.transfer(vol/20.0, ligase.bottom(), mm_well.bottom(0.5), new_tip='never')
+	p10_single.mix(2, 10, mm_well.bottom(0.5))
+	p10_single.transfer(vol/10.0, restriction_enzyme.bottom(), mm_well.bottom(0.5), new_tip='never')
+	p10_single.mix(5, 10, mm_well.bottom(0.5))
 p10_single.drop_tip()
 
 # Add master mix to each rxn
@@ -103,7 +128,7 @@ p10_single.pick_up_tip()
 mm_well = 0
 for i in range(0, num_rxns):
 	mm_well = mm_wells[i // 19]
-	p10_single.transfer(10, mm_well.bottom(), reaction_plate.wells(i).bottom(), new_tip='never')
+	p10_single.transfer(10, mm_well.bottom(0.5), reaction_plate.wells(i).bottom(0.5), new_tip='never')
 p10_single.drop_tip()
 
 def find_dna(name, dna_plate_map_dict, dna_plate_dict):
@@ -133,8 +158,9 @@ for i in combinations_to_make:
 			combinations_by_part[j] = [name]
 
 for part, combinations in combinations_by_part.items():
-	part_well = find_dna(part, dna_plate_map_dict, dna_plate_dict).bottom()
-	combination_wells = [find_combination(x, combinations_to_make).bottom() for x in combinations]
+	part_well = find_dna(part, dna_plate_map_dict, dna_plate_dict)
+	combination_wells = [find_combination(x, combinations_to_make) for x in combinations]
+	p10_single.pick_up_tip()
 	while combination_wells:
 		if len(combination_wells) > 5:
 			current_wells = combination_wells[0:5]
@@ -142,10 +168,17 @@ for part, combinations in combinations_by_part.items():
 		else:
 			current_wells = combination_wells
 			combination_wells = []
-		p10_single.aspirate(2 * len(current_wells), part_well.bottom())
+		p10_single.aspirate(2 * len(current_wells), part_well.bottom(0.5))
 		for i in current_wells:
-			p10_single.dispense(2, i.bottom())
+			p10_single.dispense(2, i.bottom(0.5))
+		if combination_wells:
+			p10_single.mix(2, 10, wash_0.bottom(0.5))
+			p10_single.blow_out()
+			p10_single.mix(2, 10, wash_1.bottom(0.5))
+			p10_single.blow_out()
+	p10_single.drop_tip()
 
+p10_single.pick_up_tip()
 for i in combinations_to_make:
 	num_parts = len(i["parts"])
 	if num_parts < 5:
@@ -153,7 +186,23 @@ for i in combinations_to_make:
 	else:
 		water_to_add = 0
 	well = find_combination(i["name"], combinations_to_make)
-	p10_single.transfer(water_to_add, water.bottom(), well.bottom(), mix_after=(4, 10))
+	p10_single.transfer(water_to_add, water.bottom(), well.bottom(0.5), new_tip='never')
+	p10_single.mix(4, 10, well.bottom(0.5))
+	p10_single.mix(2, 10, wash_0.bottom(0.5))
+	p10_single.blow_out()
+	p10_single.mix(2, 10, wash_1.bottom(0.5))
+	p10_single.blow_out()
+p10_single.drop_tip()
+
+cmds = robot.commands()
+cmds_by_type = {}
+for i in cmds:
+	type = i.split()[0]
+	if type in cmds_by_type.keys():
+		cmds_by_type[type] += 1
+	else:
+		cmds_by_type[type] = 1
+print(cmds_by_type)
 
 # Incubate rxns (moclo), periodically adding more water.
 #temp_deck.set_temperature(37)
@@ -171,17 +220,18 @@ for i in range(0, num_cols):
 	# Split up if too many rxns for one tip
 	if i % 15 == 0 and not i == 0:
 		p300_multi.dispense(300, liquid_waste)
-	p300_multi.aspirate(15, reaction_plate.wells(i*8).bottom())
+	p300_multi.aspirate(15, reaction_plate.wells(i*8).bottom(0.5))
 	p300_multi.air_gap(5)
+p300_multi.dispense(300, liquid_waste.bottom())
 p300_multi.drop_tip()
 
 # Add comp cells.
 p300_multi.pick_up_tip()
 for i in range(0, num_cols):
-	p300_multi.aspirate(50, comp_cells.wells(i*8).bottom())
+	p300_multi.aspirate(50, comp_cells.wells('A' + str(i + 1)).bottom(0.5))
 
 for i in range(0, num_cols):
-	p300_multi.dispense(50, reaction_plate.wells(i*8).bottom())
+	p300_multi.dispense(50, reaction_plate.wells(i*8).bottom(0.5))
 p300_multi.drop_tip()
 
 # Incubate at 4C, then heat shock.
@@ -194,9 +244,11 @@ p300_multi.drop_tip()
 # Add lb.
 p300_multi.pick_up_tip()
 for i in range(0, num_cols):
-	p300_multi.transfer(150, lb.bottom(), reaction_plate.wells(i*8).bottom(), mix_after=(2, 150), new_tip='never')
+	p300_multi.transfer(150, lb.bottom(), reaction_plate.wells(i*8).bottom(1), mix_after=(2, 150), new_tip='never')
 	p300_multi.mix(2, 300, wash_0.bottom())
+	p300_multi.blow_out()
 	p300_multi.mix(2, 300, wash_1.bottom())
+	p300_multi.blow_out()
 p300_multi.drop_tip()
 
 # Grow for 1 hr, adding water/mixing if necessary.
@@ -204,18 +256,22 @@ p300_multi.drop_tip()
 #p10_single.delay(minutes=60)
 
 def spread_culture(source, dest, lb, dilute_after=True):
-	p300_multi.mix(2, 150, source)
-	p300_multi.aspirate(10, source.bottom())
-	p300_multi.dispense(10, dest.top())
-	p300_multi.dispense(0, dest.bottom(-1))
+	p300_multi.mix(2, 150, source.bottom(0.5))
+	p300_multi.aspirate(10, source.bottom(0.5))
+	p300_multi.dispense(9, dest.top())
+	p300_multi.dispense(1, dest.bottom(-1))
 	if dilute_after:
-		p300_multi.transfer(120, source, liquid_waste, new_tip='never')
-		p300_multi.transfer(120, lb, source, new_tip='never')
+		p300_multi.transfer(120, source.bottom(0.5), liquid_waste.bottom(), new_tip='never')
+		p300_multi.mix(2, 300, wash_0.bottom())
+		p300_multi.blow_out()
+		p300_multi.mix(2, 300, wash_1.bottom())
+		p300_multi.blow_out()
+		p300_multi.transfer(120, lb, source.bottom(0.5), new_tip='never')
 
 # Dilute and plate.
 for i in range(0, num_cols):
 	agar_plate = agar_plates[i // 3]
-	agar_well_num = (i % 3) * 8 * 3
+	agar_well_num = (i % 3) * 8 * 4
 	p300_multi.pick_up_tip()
 	source = reaction_plate.wells(i*8)
 	spread_culture(source, agar_plate.wells(agar_well_num), lb)
